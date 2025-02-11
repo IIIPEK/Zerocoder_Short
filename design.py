44c86c5db5ff58ac80a1d215863cc0c8b0f3989f
Ivan Vorobev <ivan.vorobjov@gmail.com>
2025-02-11 22:43:23 +0200

    Init code

--- design.py
+++ design.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 
 class Field:
-    def __init__(self,window, font=("Arial", 20),width=5, height=2, size=3):
+    def __init__(self, window, font=("Arial", 20),width=5, height=2, size=3, callback=None):
         self.size = size
         self.window = window
         self.btns = []
@@ -12,20 +12,24 @@ class Field:
         self.checked = 0
         self.sign = ["X", "O"]
         self.winner = None
-        self.
+        self.callback = callback
+        self.result = None
+
 
     def check_winner(self):
+        resul = None
         for i in range(3):
             hor = "".join([char["text"] for char in self.btns[i][:]])
             ver = "".join([self.btns[j][i]["text"] for j in range(3)])
             if hor == self.sign[0] * self.size or hor == self.sign[1] * self.size:
                 return hor[0],i,"Horizontal"
-            if ver == self.sign[0] * self.size or hor == self.sign[1] * self.size:
+            elif ver == self.sign[0] * self.size or hor == self.sign[1] * self.size:
                 return ver[0],i,"Vertical"
+
         dlu =  "".join([self.btns[j][j]["text"] for j in range(self.size)])
         if dlu == self.sign[0] * self.size or dlu == self.sign[1] * self.size:
             return dlu[0],0,"LeftUp"
-        dld =  "".join([self.btns[j][self.size-j]["text"] for j in range(self.size)])
+        dld =  "".join([self.btns[j][self.size-j-1]["text"] for j in range(self.size)])
         if dld == self.sign[0] * self.size or dld == self.sign[1] * self.size:
             return dld[0],0,"LeftDown"
         return None
@@ -38,9 +42,13 @@ class Field:
         self.checked += 1
         result = self.check_winner()
         if result:
+            self.result = result
             self.winner = result[0]
         else:
             if self.checked>=9:
+                self.winner = "-"
+        if self.callback:
+            self.callback(self.result)
 
 
 
@@ -63,10 +71,28 @@ class Field:
         self.sign = [self.sign[1], self.sign[0]]
         self.field_fill()
 
+class GameWindow():
+    def __init__(self,root, size=3):
+        self.size = size
+        self.root = root
+        self.field_frame = tk.Frame(root)
+        self.field = Field(self.field_frame, size=self.size, callback=self.gameloop)
+        self.field_frame.pack()
+        self.field_frame.grid_rowconfigure(0, weight=1)
+        self.field_frame.grid_columnconfigure(0, weight=1)
+        self.field.field_fill()
+
+    def gameloop(self, result):
+        if result:
+            print(result)
+        print(self.field.winner)
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("test")
     root.geometry("300x350")
-    field = Field(root)
-    field.field_fill()
+    window = GameWindow(root)
+    # field = Field(root)
+    # field.field_fill()
     root.mainloop()
